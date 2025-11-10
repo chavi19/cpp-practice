5de16b5f425691c2f9519c4a0fe2241e2e6cda05
chavi19 <chavimaru19@gmail.com>
2025-11-10 13:38:47 +0530

    practicing patterns

---
+++ pattern.cpp
@@ -0,0 +1,46 @@
+//same seq in each line square pattern-1
+#include <iostream>
+using namespace std;
+int main() {
+    int n=3;
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=n; j++) {
+            cout<<j<<" ";
+        }
+        cout<<endl;  //w/o this output will be in the same line as 123412341234
+    }
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    for (int i=0; i<n; i++) {
+        for(char ch='A'; ch<='D'; ch++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+
+    for(int i=0; i<n; i++) {
+        char ch='a';    //should be here just before j
+        for(int j=0; j<n; j++) {
+            cout<<ch;
+            ch++;
+        }
+        cout<<endl;
+    }
+
+    int val=1;      //if it was written jst before j, val would be reset to 1 everytime
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout<<val;
+            val++;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
--- star.cpp
+++ star.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     int n=10;
     for(int i=1; i<=n; i++){        //decides no. of lines to be printed
-        int m=5;                    //below prints m no. of stars
+        int m=5;                    //below prints m no. of stars/num/alpha
         for(int j=1; j<=m; j++){
             cout<<"*";
         }
@@ -12,7 +12,7 @@ int main() {
     return 0;
 }
 /* 
-for i=1  j= 1 to m
-for i=2  j= 1 to m
+for i=1  j= 1 to m -> outer loop(lines) -> inner loop()
+for i=2  j= 1 to m 
 etc. 
 */
