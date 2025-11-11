1cf463c09e63debe4054abd9fd36f8e821d98136
chavi19 <chavimaru19@gmail.com>
2025-11-11 18:09:53 +0530

    practiced more patterns such as pyramid & hollow diamond

---
+++ hollowptn.cpp
@@ -0,0 +1,39 @@
+//HOLLOW DIAMOND PATTERN
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n=4;
+    //TOP
+    for(int i=0; i<n; i++){     //1st outer loop
+        for(int j=0; j<n-i-1; j++){  //inner loop-1 for space
+            cout<<" ";
+        }
+        cout<<"*";
+
+        if(i!=0){
+            for(int j=0; j<2*i-1; j++){    //inner loop-2 for space
+                cout<<" ";
+            }
+            cout<<"*";
+
+        }
+        cout<<endl;
+    }
+    //BOTTOM
+    for(int i=0; i<n-1; i++) {
+        for(int j=0; j<i+1; j++) {
+            cout<<" ";
+        }
+        cout<<"*";
+
+        if(i != n-2) {
+            for(int j=0; j<2*(n - i) - 5; j++){  //could be 3-2*i but only for n=4
+                cout<<" ";
+            }
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ pyramidptn.cpp
@@ -0,0 +1,19 @@
+//PYRAMID PATTERN(break into smaller triangles more loops)
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n=4;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i-1; j++) { //spaces comapre to n & i
+            cout<<" ";
+        }
+        for(int j=1; j<=i+1; j++){   //how many numbers to be printed
+            cout<<j;              //which numbers      
+        }
+        for(int j=i; j>0; j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
